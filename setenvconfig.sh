35d88a05bbec0d8f946506bcab77f1dc23847fbf
githubuserpk <krishnarao.pradeep@gmail.com>
2024-11-02 16:33:22 +0000

    Update setenvconfig.sh

--- setenvconfig.sh
+++ setenvconfig.sh
@@ -4,6 +4,7 @@ export LOCATION us-central1
 export REGION us-central1
 export REPOSITORY=ollama-repo
 export OLLAMA_IDENTITY=ollama-sa
+export SERVICE=ollama-gemma
 
 gcloud config set project $PROJECT
 gcloud config set run/region $REGION
