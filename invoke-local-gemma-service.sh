0e3f55dbcec7460d87e907007ed697d19d226913
githubuserpk <krishnarao.pradeep@gmail.com>
2024-11-02 21:31:10 +0000

    Delete invoke-local-gemma-service.sh

--- invoke-local-gemma-service.sh
+++
@@ -1,9 +0,0 @@
-## invoke the ollama gemma service 
-
-gcloud run services proxy ollama-gemma --port=9090
-
-curl http://localhost:9090/api/generate -d '{
-  "model": "gemma2:9b",
-  "prompt": "Why is the sky blue?"
-}'
-
